d21d058cea8a7023edb37849b719ef5efb991efe
Tianyang <1538109712@qq.com>
2023-01-12 13:15:35 +0100

    add attention

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # 按 ⌃R 执行或将其替换为您的代码。
 # 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-
+import os.path as osp
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
@@ -12,3 +10,6 @@ def print_hi(name):
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(__file__)
+    print(__name__)
+    print(osp.dirname(__file__))
